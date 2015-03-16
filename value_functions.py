dc4dc7d390b86ce2cf868bce74b4eed6512c6db5
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-03-16 23:12:09 +0000

    Add a way to convert an action value function to value function

--- value_functions.py
+++ value_functions.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+HIT, STICK = 1, 0
+
 # A value function is simply a map from state -> value
 class Value(defaultdict):
     def __getitem__(self, (dealer, player)):
@@ -16,5 +18,24 @@ class ActionValue(defaultdict):
 
     def __setitem__(self, (dealer, player, action), value):
         dict.__setitem__(self, (dealer, player, action), value)
+
+    # converts an action value function to a value function by
+    # simply creating a new value function and adding either the value
+    # of hitting ir sticking, deending on which is better
+    def to_value_function(self):
+        value_function = Value(float)
+        keys = self.keys()
+
+        for key in keys:
+            dealer, player, action = key[0], key[1], key[2]
+            hit_value = self.get((dealer, player, HIT))
+            stick_value = self.get((dealer, player, STICK))
+
+            if hit_value > stick_value:
+                value_function[(dealer, player)] = hit_value
+            else:
+                value_function[(dealer, player)] = stick_value
+
+        return value_function
 
 
